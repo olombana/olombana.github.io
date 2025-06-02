f0a931c301533a6dca839cbf65bc3e0feb88bd80
Oscar Lombana <42478242+olombana@users.noreply.github.com>
2025-06-02 12:50:03 -0500

    update

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -40,12 +40,12 @@ export type SiteConfig = {
 
 const siteConfig: SiteConfig = {
     website: 'https://example.com',
-    title: 'Dante',
-    subtitle: 'Minimal Astro.js theme',
-    description: 'Astro.js and Tailwind CSS theme for blog and portfolio by justgoodui.com',
+    title: 'Oscar Lombana',
+    subtitle: 'Data Analytics',
+    description: 'Bienvenidos a mi espacio donde les compartiré mis proyectos y temas de Analytics',
     image: {
         src: '/dante-preview.jpg',
-        alt: 'Dante - Astro.js and Tailwind CSS theme'
+        alt: 'Este no es Oscar Lombana'
     },
     headerNavLinks: [
         {
@@ -77,10 +77,6 @@ const siteConfig: SiteConfig = {
         {
             text: 'Terms',
             href: '/terms'
-        },
-        {
-            text: 'Download theme',
-            href: 'https://github.com/JustGoodUI/dante-astro-theme'
         }
     ],
     socialLinks: [
@@ -99,7 +95,7 @@ const siteConfig: SiteConfig = {
     ],
     hero: {
         title: 'Hi There & Welcome to My Corner of the Web!',
-        text: "I'm **Ethan Donovan**, a web developer at Amazing Studio, dedicated to the realms of collaboration and artificial intelligence. My approach involves embracing intuition, conducting just enough research, and leveraging aesthetics as a catalyst for exceptional products. I have a profound appreciation for top-notch software, visual design, and the principles of product-led growth. Feel free to explore some of my coding endeavors on <a href='https://github.com/JustGoodUI/dante-astro-theme'>GitHub</a> or follow me on <a href='https://twitter.com/justgoodui'>Twitter/X</a>.",
+        text: "Soy **Oscar Lombana*, no soy la persona de la foto de arriba (Prometo que pronto la actualizaré) pues este es mi sandbox para jugar con diferentes plataformas de Analítica Digital, pero si soy un entusiasta de los datos y la analítica por lo que les iré compartiendo por aquí algunos temas interesantes.",
         image: {
             src: '/hero.jpeg',
             alt: 'A person sitting at a desk in front of a computer'
